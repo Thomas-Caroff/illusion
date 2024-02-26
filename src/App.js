f534afe9f53a7b1916aaf54064ad3a916820cb8b
thomas.caroff <[thomasc97@hotmail.fr]>
2024-02-26 14:55:22 +0100

    [UI + DEV] Creating basic sidebar and header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component, Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import AppLayout from "./components/AppLayout";
+import Blank from "./pages/Blank";
+import "boxicons/css/boxicons.min.css";
 import "./assets/css/app.css";
 
 class App extends Component {
@@ -8,7 +12,17 @@ class App extends Component {
 		return (
 			<Fragment>
 				<Header />
-				<Home />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<AppLayout />}>
+							<Route index element={<Home />} />
+							<Route path="/started" element={<Blank />} />
+							<Route path="/calendar" element={<Home />} />
+							<Route path="/user" element={<Blank />} />
+							<Route path="/order" element={<Home />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
 			</Fragment>
 		);
 	}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "../assets/css/header.css";
 
 class Header extends Component {
 	render() {
 		return (
-			<div className="text-center">
+			<div className="header">
 				<h1>App with React + Django</h1>
 			</div>
 		);
