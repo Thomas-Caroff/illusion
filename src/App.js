3ccef927664f513d7093f120d814922879b61720
thomas.caroff <[thomasc97@hotmail.fr]>
2024-06-04 00:51:53 +0200

    [REFACTOR] Adding MD template, connecting to API

--- src/App.js
+++ src/App.js
@@ -1,36 +1,210 @@
-import React, { Component, Fragment } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import AppLayout from "./components/AppLayout";
-import Blank from "./pages/Blank";
-import "boxicons/css/boxicons.min.css";
-import "./assets/css/app.scss";
-import { Container, Row } from "reactstrap";
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
 
-class App extends Component {
-	render() {
-		return (
-			<Container>
-				<Row>
-					<Header />
-				</Row>
-				<Row>
-					<BrowserRouter>
-						<Routes>
-							<Route path="/" element={<AppLayout />}>
-								<Route index element={<Home />} />
-								<Route path="/started" element={<Blank />} />
-								<Route path="/calendar" element={<Home />} />
-								<Route path="/user" element={<Blank />} />
-								<Route path="/order" element={<Home />} />
-							</Route>
-						</Routes>
-					</BrowserRouter>
-				</Row>
-			</Container>
-		);
-	}
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState, useEffect, useMemo } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Icon from "@mui/material/Icon";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import Sidenav from "examples/Sidenav";
+import Configurator from "examples/Configurator";
+
+// Material Dashboard 2 React themes
+import theme from "assets/theme";
+import themeRTL from "assets/theme/theme-rtl";
+
+// Material Dashboard 2 React Dark Mode themes
+import themeDark from "assets/theme-dark";
+import themeDarkRTL from "assets/theme-dark/theme-rtl";
+
+// RTL plugins
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
+// Material Dashboard 2 React routes
+import routes from "routes";
+
+// Material Dashboard 2 React contexts
+import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
+
+// Images
+import brandWhite from "assets/images/logo-ct.png";
+import brandDark from "assets/images/logo-ct-dark.png";
+import { API_URL } from "constants/index";
+
+export default function App() {
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    direction,
+    layout,
+    openConfigurator,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+  } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const [rtlCache, setRtlCache] = useState(null);
+  const { pathname } = useLocation();
+
+  // Cache for the rtl
+  useMemo(() => {
+    const cacheRtl = createCache({
+      key: "rtl",
+      stylisPlugins: [rtlPlugin],
+    });
+
+    setRtlCache(cacheRtl);
+  }, []);
+
+  // Open sidenav when mouse enter on mini sidenav
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  // Close sidenav when mouse leave mini sidenav
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  // Change the openConfigurator state
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+
+  // Setting the dir attribute for the body element
+  useEffect(() => {
+    document.body.setAttribute("dir", direction);
+  }, [direction]);
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  useEffect(() => {
+    // Fetch our data from our API
+    fetch(API_URL + "user/")
+      // Resolve the responsove data
+      .then((response) => response.json())
+      .then((json) => {
+        // Show data in console
+        console.log(json);
+      });
+  }, []);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
+  const configsButton = (
+    <MDBox
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="3.25rem"
+      height="3.25rem"
+      bgColor="white"
+      shadow="sm"
+      borderRadius="50%"
+      position="fixed"
+      right="2rem"
+      bottom="2rem"
+      zIndex={99}
+      color="dark"
+      sx={{ cursor: "pointer" }}
+      onClick={handleConfiguratorOpen}
+    >
+      <Icon fontSize="small" color="inherit">
+        settings
+      </Icon>
+    </MDBox>
+  );
+
+  return direction === "rtl" ? (
+    <CacheProvider value={rtlCache}>
+      <ThemeProvider theme={darkMode ? themeDarkRTL : themeRTL}>
+        <CssBaseline />
+        {layout === "dashboard" && (
+          <>
+            <Sidenav
+              color={sidenavColor}
+              brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+              brandName="Material Dashboard 2"
+              routes={routes}
+              onMouseEnter={handleOnMouseEnter}
+              onMouseLeave={handleOnMouseLeave}
+            />
+            <Configurator />
+            {configsButton}
+          </>
+        )}
+        {layout === "vr" && <Configurator />}
+        <Routes>
+          {getRoutes(routes)}
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </ThemeProvider>
+    </CacheProvider>
+  ) : (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+      <CssBaseline />
+      {layout === "dashboard" && (
+        <>
+          <Sidenav
+            color={sidenavColor}
+            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+            brandName="Material Dashboard 2"
+            routes={routes}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+          />
+          <Configurator />
+          {configsButton}
+        </>
+      )}
+      {layout === "vr" && <Configurator />}
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
-
-export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,63 +5,51 @@ import UserList from "../components/user/UserList";
 import NewUserModal from "../components/user/NewUserModal";
 import Calendar from "../components/calendar/Calendar";
 import Widget from "../components/widget/Widget";
-import { API_URL } from "../constants";
+import { API_URL } from "../constants/index";
 import "../assets/css/app.scss";
 
 class Home extends Component {
-	state = {
-		users: [],
-	};
+  state = {
+    users: [],
+  };
 
-	componentDidMount() {
-		this.resetState();
-	}
+  componentDidMount() {
+    this.resetState();
+  }
 
-	getUsers = () => {
-		axios
-			.get(API_URL + "user/")
-			.then((res) => this.setState({ users: res.data }));
-	};
+  getUsers = () => {
+    axios.get(API_URL + "user/").then((res) => this.setState({ users: res.data }));
+    console.log(this.state.users);
+  };
 
-	resetState = () => {
-		this.getUsers();
-	};
+  resetState = () => {
+    this.getUsers();
+  };
 
-	render() {
-		return (
-			<Container className="body">
-				<Row>
-					<Col>
-						<UserList
-							users={this.state.users}
-							resetState={this.resetState}
-						/>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<NewUserModal
-							create={true}
-							resetState={this.resetState}
-						/>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<Widget
-							title={<h6>Calendar</h6>}
-							settings
-							close
-							bodyClass={"pt-2 px-0 py-0"}
-						>
-							<Calendar />
-						</Widget>
-					</Col>
-				</Row>
-				<div>Test</div>
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <Container className="body">
+        <Row>
+          <Col>
+            <UserList users={this.state.users} resetState={this.resetState} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <NewUserModal create={true} resetState={this.resetState} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Widget title={<h6>Calendar</h6>} settings close bodyClass={"pt-2 px-0 py-0"}>
+              <Calendar />
+            </Widget>
+          </Col>
+        </Row>
+        <div>Test</div>
+      </Container>
+    );
+  }
 }
 
 export default Home;
